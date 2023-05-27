5712395eb36070f6fd5a624730e594b2e353084c
hamid reza <reza.attar1375@gmail.com>
2023-05-27 23:38:17 +0430

    bottomTab

--- src/other/Layout/Layout.js
+++ src/other/Layout/Layout.js
@@ -13,12 +13,11 @@ import SellerPage from './page/SellersPage';
 import SellerPanelPage from './page/SellerPanelPage';
 import AddressPage from './page/AddressPage';
 import BeforePaymentPage from './page/BeforePaymentPage';
+import SocketIoPage from './page/SocketIoPage';
 import ProductsOffersPage from './page/ProductsOffersPage';
 import ProductsPopularsPage from './page/ProductsPopularsPage';
 
-
 export const Layout = (p) => {
-
 
   useFocusEffect(useCallback(() => {
     if (p._key === '1') { p.sethomeNavigate(); p.sethomeParams() }
@@ -26,7 +25,6 @@ export const Layout = (p) => {
       if (p._key === '1') { p.sethomeNavigate(p.route.name); p.sethomeParams(p.route.params) }
     }
   }, []))
-
 
   const topUser = [{ name: 'Register', title: 'ثبت نام' }, { name: 'Login', title: 'ورود' }]
 
@@ -65,29 +63,32 @@ export const Layout = (p) => {
           p.route.name === 'ProductsPopulars' &&
           <ProductsPopularsPage {...p} bottom={bottom} />
           ||
-          p.route.name === 'ProductsTable' &&
-          <ProductsTablePage {...p} bottom={bottom} />
-          ||
           p.route.name === 'SingleProduct' &&
           <SingleProductPage {...p} bottom={bottom} />
           ||
-          (p.route.params?.key === 'admin') && (!p.route.params?.set) && (p.route.name !== 'Address') && (p.route.name !== 'Sellers') &&
-          <PanelAdminPage {...p} bottom={bottom} />
-          ||
-          (p.route.params?.key === 'user') && (!p.route.params?.view) && (p.route.name !== 'SellerPanel') && (!p.route.params?.active) &&
-          <ProfilePage {...p} bottom={bottom} />
-          ||
-          p.route.name === 'Sellers' &&
-          <SellerPage {...p} bottom={bottom} />
-          ||
-          p.route.name === 'SellerPanel' &&
-          <SellerPanelPage {...p} bottom={bottom} />
-          ||
-          p.route.name === 'Address' &&
-          <AddressPage {...p} bottom={bottom} />
-          ||
           p.route.name === 'BeforePayment' &&
           <BeforePaymentPage {...p} bottom={bottom} />
+          ||
+          p.route.name === 'SocketIo' &&
+          <SocketIoPage {...p} bottom={bottom} />
+          ||
+          p.route.name === 'ProductsTable' &&
+          <ProductsTablePage {...p} />
+          ||
+          (p.route.params?.key === 'admin') && (!p.route.params?.set) && (p.route.name !== 'Address') && (p.route.name !== 'Sellers') &&
+          <PanelAdminPage {...p} />
+          ||
+          (p.route.params?.key === 'user') && (!p.route.params?.view) && (p.route.name !== 'SellerPanel') && (!p.route.params?.active) &&
+          <ProfilePage {...p} />
+          ||
+          p.route.name === 'Sellers' &&
+          <SellerPage {...p} />
+          ||
+          p.route.name === 'SellerPanel' &&
+          <SellerPanelPage {...p} />
+          ||
+          p.route.name === 'Address' &&
+          <AddressPage {...p} />
           ||
           <ContainerTab >{p.children}</ContainerTab>
         }
@@ -101,11 +102,7 @@ export const header = () => {
   const navigation = useNavigation();
   return (
     <Pressable onPress={() => navigation.goBack()} >
-      <Icon
-        name='arrow-right'
-        style={{ fontSize: 29, marginTop: -5, paddingVertical: 2.5, flexGrow: 1, color: '#222', fontWeight: '1000' }} />
-
-
+      <Icon name='arrow-right' style={{ fontSize: 29, marginTop: -5, paddingVertical: 2.5, flexGrow: 1, color: '#222', fontWeight: '1000' }} />
     </Pressable>
   );
 };
