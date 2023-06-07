466f9683bfb0ade51f2f17fa408df744fde09324
hamid reza <reza.attar1375@gmail.com>
2023-06-07 20:20:40 +0430

    pwaEnd

--- public/app.js
+++ public/app.js
@@ -1,44 +1,20 @@
-function urlBase64ToUint8Array(base64String) {
-  const padding = '='.repeat((4 - base64String.length % 4) % 4);
-  const base64 = (base64String + padding)
-    .replace(/-/g, '+')
-    .replace(/_/g, '/');
+const APP = {
+  deferredInstall: null,
+  init() {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/sw.js', {
+          updateViaCache: 'none',
+          scope: '/',
+        })
+        .then((rg) => {
+          console.warn('then register', rg);
 
-  const rawData = window.atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
-
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-  return outputArray;
-}
-
-const publicVapidKey = 'BM-KLhqHaocRV4wT-3gp1RzWqj3PhlKPmpWrihXpaYgzfKF3KRfN9ZGR3nRz7p-lCBAKvH80qb4UOcFIorjkTP4';
-
-
-async function triggerPushNotification() {
-  if ('serviceWorker' in navigator) {
-    const register = await navigator.serviceWorker.register('/sw.js', {
-      scope: '/'
-    });
-
-    const subscription = await register.pushManager.subscribe({
-      userVisibleOnly: true,
-      applicationServerKey: urlBase64ToUint8Array(publicVapidKey),
-    });
-
-    await fetch('http://localhost:4000/createNotification2', {
-      method: 'POST',
-      body: JSON.stringify({subscription:subscription, title:'title', message:'message'}),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  } else {
-    console.error('Service workers are not supported in this browser');
-  }
-}
-
-document.addEventListener('DOMContentLoaded', ()=>{triggerPushNotification().catch(error => console.error(error));});
-
-
+        })
+        .catch((err) => {
+          console.warn('Failed to register', err.message);
+        });
+    }
+  },
+};
+document.addEventListener('DOMContentLoaded', APP.init);
