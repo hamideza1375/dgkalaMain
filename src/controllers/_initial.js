0edfb33c80758a0967bc80dc180c4aabfd4cb755
hamid reza <reza.attar1375@gmail.com>
2023-06-20 11:11:59 +0430

    backhandleend2

--- src/controllers/_initial.js
+++ src/controllers/_initial.js
@@ -146,17 +146,26 @@ export function allChildren({ client, user, admin }) {
     }, 150);
   }, [netInfo])
 
+  // const b = () => {
+  //   console.log(location.href);
+  //   if ((location.href === (location.origin + '/home')) || (location.href === (location.origin)) || (location.href === (location.origin + '/'))) { num++; setTimeout(() => { num = 0 }, 1000) };
+  //   if (num === 2) { client.toast.show('', 'برای خروج دوبار کلیک کنید', 1000); setTimeout(() => { num = 0 }, 1000); }
+  //   if (num >= 2) { setTimeout(() => {history.go(-(history.length - 1)) ; history.back()}, 100); return }
+  //   if ((location.href === location.origin) || (location.href === location.origin + '/')) { history.go(-(history.length - 1)) ; history.back() }
+  //   else return
+  // }
+
   const b = () => {
     if ((location.href === (location.origin + '/home')) || (location.href === (location.origin)) || (location.href === (location.origin + '/'))) { num++; setTimeout(() => { num = 0 }, 1000) };
-    if (num === 1) { (a > 0) && client.toast.show('', 'برای خروج دوبار کلیک کنید', 1000); setTimeout(() => { num = 0 }, 1000); }
-    if (num >= 2) { { history.go(-(history.length - 1)); history.back() } return }
-    if ((location.href === location.origin) || (location.href === location.origin + '/')) { history.go(-(history.length - 1)); history.back() }
+    if (num === 1) { if(a > 0) {client.toast.show('', 'برای خروج دوبار کلیک کنید', 1000); setTimeout(() => { num = 0 }, 1000);} else return }
+    else if (num >= 2) { { history.go(-(history.length - 1)); history.back() } return }
+    else if ((location.href === location.origin) || (location.href === location.origin + '/')) { history.go(-(history.length - 1)); history.back() }
     else return
+    return
   }
 
   _useEffect(() => { if (Platform.OS === 'web') { if (location.href === location.origin + '/' || location.href === location.origin + '/home') history.pushState({}, '/home'/* location.href */) } }, [])
-  _useEffect(() => { if (Platform.OS === 'web') window.addEventListener('popstate', b); return () => { if (Platform.OS === 'web') { num = 0; window.removeEventListener('popstate', b); } } }, [])
-
+  _useEffect(() => { if (Platform.OS === 'web') window.addEventListener('popstate', b); return () => { if (Platform.OS === 'web') { num = 0; /* window.removeEventListener('popstate', b); */ } } }, [])
 
   const _client = ({ navigation, route }) => new clientController({ ...client, navigation, route })
   const _user = ({ navigation, route }) => new userController({ ...user, navigation, route })
@@ -166,12 +175,11 @@ export function allChildren({ client, user, admin }) {
   const adminReducer = (props) => ({ _admin: _admin(props) })
   this.clientChildren = (Component, key) => ({
     children: (props) => {
-
       _useEffect(() => {
-        a = 0
-        if ((location.href === location.origin + '/') || (location.href === location.origin + '/home') || props.route.name === 'Home' ) setTimeout(() => {a++}, 200);
-      },[])
-
+        if (props.route.name === 'Home') setTimeout(() => { a++; }, 300);
+        else a = 0
+        return () => a = 0
+      }, [])
       _useEffect(() => { AsyncStorage.getItem("token").then((token) => { if ((props.route.name === 'SetAddressForm' || props.route.name === 'SetAddressInTehran' || props.route.name === 'ProductBasket') && (!token)) props.navigation.navigate('User', { screen: 'Login', params: { payment: 'true' } }); }) }, [])
       _useEffect(() => { client.setshownDropdown(false); }, [])
       useLayoutEffect(() => { if (props.route.params?.id && !idValidator(props.route.params.id)) return props.navigation.navigate('NotFound') })
